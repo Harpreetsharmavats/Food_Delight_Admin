bac3429c20b643fc59ae9ea73a29e3bdfc27be16
Harpreetsharmavats <harpreetsharma902@gmail.com>
2025-01-14 19:51:45 +0530

    Initial commit

---
+++ app/src/main/java/com/example/fooddelightadmin/SplashScreenActivity.kt
@@ -0,0 +1,19 @@
+package com.example.fooddelightadmin
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.fooddelightadmin.databinding.ActivitySplashScreenBinding
+
+class SplashScreenActivity : AppCompatActivity() {
+    private val binding: ActivitySplashScreenBinding by lazy {
+        ActivitySplashScreenBinding.inflate(layoutInflater)
+    }
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(binding.root)
+
+    }
+}
